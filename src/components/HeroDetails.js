fd55d825d958aae88267863bad3fb0a0605407fa
MurilloAvellaneda <murillo.avellaneda@gmail.com>
2022-02-19 11:05:54 -0300

    adicionando css ao HeroDetails

--- src/components/HeroDetails.js
+++ src/components/HeroDetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import apiCharacters from '../utils/apiCharacters';
+import "./HeroDetails.css"
 
 const HeroDetails = () => {
 
@@ -22,28 +23,37 @@ const HeroDetails = () => {
       <>
         {character.thumbnail ?
         <div>
-            <img 
+          <div className = "container">
+            <h1>{character.name}</h1>
+            <div className = "character-img-series">
+              <img 
                 src={`${character.thumbnail.path}.${character.thumbnail.extension}`} 
                 alt='character'
-                style={{width: '400px', margin:'2px 10px 2px 2px'}}
-            />
-            <h1>{character.name}</h1>
-            <h2>{character.description}</h2>
-            <ul>
-            {character.series.items.map((series, index) => {
-                return (
-                    <li 
-                        key = {character.id, index}> 
-                        {series.name} 
-                    </li>)
-                    }
-                )
-            }
-        </ul>  
-        </div>
+                style={{width: '450px', margin:'2px 10px 2px 2px'}}
+              />
+              <ul className = "series-list">
+              <li>Series List</li>
+              {character.series.items.map((series, index) => {
+                  return (
+                      <li className = "serie"
+                          key = {character.id, index}> 
+                          {series.name} 
+                      </li>)
+                      }
+                  )
+              }
+              </ul>
+            </div>
+          </div>
+          <div className = "description">    
+            <div className = "container" >  
+                <h2>{character.description}</h2>
+            </div>
+          </div>
+        </div> 
         : "loading"
-    }
-    </>
+        }
+      </>
   )
 }
 
